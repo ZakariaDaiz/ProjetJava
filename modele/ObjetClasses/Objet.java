4469aae0571553e7498de193781644c29f1599f7
BRIOUL Vivien <brioulvivien@gmail.com>
2025-12-15 22:18:25 +0100

    Changements ObjetFactoryFuturiste

--- modele/ObjetClasses/Objet.java
+++ modele/ObjetClasses/Objet.java
@@ -1,9 +1,12 @@
 package modele.ObjetClasses;
 
+import modele.Personnage;
+
 public abstract class Objet {
     private String nom;
     private String description;
 
-    public void utiliser(Personnage cible);
+    public void utiliser(Personnage cible){
 
+    }
 }
